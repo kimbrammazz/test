60ec41ad3b346e3234e831affc80391205a22397
kimbrammazz <kimbrammazz@gmail.com>
2022-06-09 10:46:56 +1000

    added kinde.js

---
+++ js/kinde.js
@@ -0,0 +1,41 @@
+import createKindeClient from '@kinde-oss/kinde-auth-pkce-js';
+
+const kinde = await createKindeClient({
+    domain: 'test.kinde.com',
+    redirect_uri: window.location.origin
+});
+
+document.getElementById('login').addEventListener('click', async () => {
+    console.log("pressed login button");
+  await kinde.login();
+});
+
+document.getElementById('register').addEventListener('click', async () => {
+  await kinde.register();
+});
+
+
+window.addEventListener('load', async () => {
+  await kinde.handleRedirectCallback();
+  const user = await kinde.getUser();
+  console.log({user});
+});
+
+document.getElementById('logout').addEventListener('click', async () => {
+    await kinde.logout();
+});
+
+(async () => {
+    try {
+        const token = await kinde.getToken();
+        const response = await fetch(YOUR_API, {
+            headers: new Headers({
+                Authorization: 'Bearer ' + token
+            })
+        });
+        const data = await response.json();
+        console.log({data});
+    } catch (err) {
+        console.log(err);
+    }
+})();
